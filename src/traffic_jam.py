d777bfba7257b69a1c9a1b9b3cb0e224c1188648
BaptisteRdt <rodetbaptiste@gmail.com>
2024-04-09 18:35:55 +0200

    [upd] traffic jam function

--- src/traffic_jam.py
+++ src/traffic_jam.py
@@ -1,42 +1,79 @@
-import cv2
-from ultralytics import YOLO
+from math import hypot
 
-# Load the YOLOv8 model
-model = YOLO('models/best_model_Yv8_epoch41.pt')
+types = {0: "car", 1: "motorcycle", 2: "truck"}
 
-# Open the video file
-video_path = ("../../src/data/croisement.mp4")
 
-cap = cv2.VideoCapture(video_path)
+def get_vehicles_dict(results) -> dict:
+    vehicles = {}
+    for bbox in results[0].boxes:
+        xl, yt, xr, yb = bbox.xyxyn[0]
+        vehicles[int(bbox.id)] = {'type': types[int(bbox.cls)], 'xl': float(xl), 'yt': float(yt),
+                                  'xr': float(xr), 'yb': float(yb), 'confidence': float(bbox.conf),
+                                  'center': {'x': float(xl + xr) / 2, 'y': float(yt + yb) / 2},
+                                  'is_track': bbox.is_track, 'id': int(bbox.id)}
+    return vehicles
 
-# frame rate
-frame_rate = 5
-i = 0
 
-# Loop through the video frames
-while cap.isOpened():
-    # Read a frame from the video
-    success, frame = cap.read()
+def get_closest_vehicles(vehicles: dict) -> dict:
+    for vehicle in vehicles.values():
+        if vehicle['is_track']:
+            min_distance = 1
+            for other_vehicle in vehicles.values():
+                if vehicle['id'] != other_vehicle['id']:
+                    distance = hypot(abs(vehicle['center']['x'] - other_vehicle['center']['x']),
+                                     abs(vehicle['center']['y'] - other_vehicle['center']['y']))
+                    if distance < min_distance:
+                        min_distance = distance
+                        vehicle['id_closest_vehicle'] = other_vehicle['id']
+    return vehicles
 
-    if success:
-        if i % frame_rate == 0:
-            # Run YOLOv8 tracking on the frame, persisting tracks between frames
-            results = model.track(frame, persist=True)
-            print(results)
-            # Visualize the results on the frame
-            annotated_frame = results[0].plot()
-            annotated_frame = cv2.resize(annotated_frame, None, fx=0.5, fy=0.5)
-            # Display the annotated frame
-            cv2.imshow("YOLOv8 Tracking", annotated_frame)
 
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+def direction_of_closest_vehicle(vehicle, closest_vehicle) -> dict:
+    diff_x = vehicle['center']['x'] - closest_vehicle['center']['x']
+    diff_y = vehicle['center']['y'] - closest_vehicle['center']['y']
+
+    direction = {'Top': 0,'Bottom': 0,'Left': 0,'Right': 0}
+    if diff_y < 0.0:
+        direction['Top'] = 1
     else:
-        # Break the loop if the end of the video is reached
-        break
-    i += 1
+        direction['Bottom'] = 1
+    if diff_x < 0.0:
+        direction['Left'] = 1
+    else:
+        direction['Right'] = 1
+    return direction
 
-# Release the video capture object and close the display window
-cap.release()
-cv2.destroyAllWindows()
+
+def get_distance_from_closest_vehicle(vehicle, closest_vehicle, direction) -> float:
+    if direction['Top'] == 1:
+        distance_y = vehicle['yt'] - closest_vehicle['yt']
+    if direction['Bottom'] == 1:
+        distance_y = vehicle['yb'] - closest_vehicle['yb']
+    if direction['Left'] == 1:
+        distance_x = vehicle['xl'] - closest_vehicle['xl']
+    if direction['Right'] == 1:
+        distance_x = vehicle['xr'] - closest_vehicle['xr']
+
+    return (distance_x + distance_y) / 2
+
+
+def traffic_jam_condition(vehicles: dict, threshold: float) -> bool:
+    close_vehicles = 0
+    for vehicle in vehicles.values():
+        if vehicle['id_closest_vehicle'] is not None:
+            closest_vehicle = vehicles[vehicle['id_closest_vehicle']]
+            direction_of_vehicle = direction_of_closest_vehicle(vehicle, closest_vehicle)
+            distance = get_distance_from_closest_vehicle(vehicle, closest_vehicle, direction_of_vehicle)
+            if distance < threshold:
+                close_vehicles += 1
+    if close_vehicles >= len(vehicles)*0.8:
+        return True
+    else:
+        return False
+
+
+def traffic_jam(results, threshold: float) -> bool:
+    vehicles = get_vehicles_dict(results)
+    vehicles = get_closest_vehicles(vehicles)
+
+    return traffic_jam_condition(vehicles, threshold)
